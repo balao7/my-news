9c538bab79f10c7bec7eb93d68a00ff7a1a2db68
Eric N <eric.nguyen@ITSG001503-MAC.local>
2018-09-03 00:59:02 +0800

    Removed more unnecessary stuffs

--- app/src/main/java/com/eric/mynews/AppModule.java
+++ app/src/main/java/com/eric/mynews/AppModule.java
@@ -8,18 +8,19 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 @Module(includes = {RepositoryModule.class})
 public class AppModule {
     @Provides
     @Singleton
     Retrofit.Builder provideRetrofit() {
-        final OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new HttpLoggingInterceptor().setLevel
-                (HttpLoggingInterceptor.Level.BODY))
-                .build();
-        return new Retrofit.Builder().client(client)
-                .addConverterFactory(GsonConverterFactory.create())
+        final OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder();
+        if (BuildConfig.DEBUG) {
+            okHttpBuilder.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
+        }
+        return new Retrofit.Builder().client(okHttpBuilder.build())
+                .addConverterFactory(MoshiConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
     }
 }
